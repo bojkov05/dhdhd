c7a2e363ed95849c8b09431bf82803b01017b09d
PTF <112647616+bojkov05@users.noreply.github.com>
2023-09-18 11:45:44 +0300

    Update ok.jl

--- ok.jl
+++ ok.jl
@@ -1,5 +1,43 @@
 using HorizonSideRobots
-r = Robot(animate = true)
-move!(r,Nord)
-move!(r,Nord)
-move!(r,Nord)
+r=Robot(animate = true)
+back=[]
+function long(r,side::Array,mark::Bool)
+    c=[]
+    for s in side
+        while !isborder(r,s)
+            move!(r,s)
+            if mark
+                putmarker!(r)
+            end
+            push!(c,s)
+        end
+    end
+    return c
+end
+
+back=long(r,[Nord,Ost],true)
+
+function rever(side)
+    if side == Nord
+        return Sud
+    end
+    if side == Sud
+        return Nord
+    end
+    if side == West
+        return Ost
+    end
+    if side == Ost
+        return West
+    end
+end
+
+function Home(back::Array)
+    back=reverse(back)
+    for s in back
+        print(rever(s))
+        move!(r,rever(s))
+    end
+end
+
+Home(back)
